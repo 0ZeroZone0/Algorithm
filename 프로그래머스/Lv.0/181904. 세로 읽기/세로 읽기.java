8a327441b7c5840bc086bdf10663312413af1e47
0Zone <109025429+0ZeroZone0@users.noreply.github.com>
2024-01-11 21:36:00 +0900

    [level Lv.0] Title: 세로 읽기, Time: 1.28 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181904. 세로 읽기/세로 읽기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        List<String> result = new ArrayList<>();
+        
+        for (int i = 0; i < my_string.length(); i += m) {
+            String str = my_string.substring(i, i + m);
+            result.add(str);
+        }
+
+        
+        String answer = "";
+        for (String res : result) {
+            answer += res.substring(c-1, c);
+        }
+        
+        return answer;
+    }
+}
